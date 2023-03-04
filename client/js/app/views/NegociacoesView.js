e57b747a658e2addb07128978ea9c7c29d00c6ec
Allan M Alegretti <allanalegretti@gmail.com>
2023-03-04 08:59:33 -0300

    Arrumando a msg no DOM e heranca em views

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,8 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
     constructor(elemento) {
-
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model) {
@@ -39,8 +38,4 @@ class NegociacoesView {
         `;
     }
 
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
-    }
 }
